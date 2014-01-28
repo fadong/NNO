7b6929eeee848ca9d17ea2254c322c0bc54d407b
fadong <jaekyoon.lee@gmail.com>
2014-01-28 22:53:52 +0900

    Client 추가

--- NNO/Program.cs
+++ NNO/Program.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace NNO {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("a");
-            Console.WriteLine("a");
+            
+
+
 
         }
     }
